21b1053e489b4bec2791c7e65a5d638226de4b08
SayedTabish72 <sayedtabish72@gmail.com>
2023-07-04 23:33:46 +0530

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/SayedTabish72/AI_articles_Summarizer/tree/main", "_blank")
           }
           className='black_btn'
         >
